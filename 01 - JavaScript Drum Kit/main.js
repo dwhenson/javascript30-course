624a540c81954c97beeec9a7824419175a9e39cf
Dave Henson <dave.henson@gmail.com>
2020-09-14 21:33:15 +0300

    ⚡ADD: add and remove class indicating key pressed

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -4,14 +4,18 @@
 	const keys = [...document.querySelectorAll("div[data-key]")];
 
 	/* ==========  Functions  ========== */
-	function keyHandler(event) {
+	function keyDownHandler(event) {
 		keys.forEach((key) => {
-			if (event.which === parseInt(key.getAttribute("data-key"), 10)) {
-				document.querySelector([`audio[data-key="${event.which}"]`]).play();
-			}
+			if (!event.which === parseInt(key.getAttribute("data-key"), 10)) return;
+			document.querySelector([`audio[data-key="${event.which}"]`]).play();
+			const drum = document.querySelector([`div[data-key="${event.which}"]`]);
+			drum.classList.add("playing");
+			setTimeout(() => {
+				drum.classList.remove("playing");
+			}, 70);
 		});
 	}
 
 	/* ==========  Inits and Event Listeners  ========== */
-	document.addEventListener("keydown", keyHandler);
+	document.addEventListener("keydown", keyDownHandler);
 })();
