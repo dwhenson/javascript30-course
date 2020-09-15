aeddf2113d953b22eb127080a5f0e4fc262617fc
Dave Henson <dave.henson@gmail.com>
2020-09-15 20:32:59 +0300

    ⚡ ADD: complete second hand movement

--- 02 - JS and CSS Clock/main.js
+++ 02 - JS and CSS Clock/main.js
@@ -1,4 +1,15 @@
-// Avoid global scope
-(function () {
-	console.log("It works");
-})();
+/* ==========  Variables  ========== */
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+/* ==========  Functions  ========== */
+function setDate() {
+	const now = new Date();
+	// second hand
+	const seconds = now.getSeconds();
+	const secondDegrees = (seconds / 60) * 360 + 90;
+	secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
