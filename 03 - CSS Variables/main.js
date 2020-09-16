2fe7bd0a7dbf6f03baff6ddd102dffb64991df72
Dave Henson <dave.henson@gmail.com>
2020-09-16 08:36:24 +0300

    🚀 RELEASE: final solution day 3
    Followed tutorial rather than own code as was unclear how to aproach this one

--- 03 - CSS Variables/main.js
+++ 03 - CSS Variables/main.js
@@ -1,4 +1,15 @@
-// Avoid global scope
-(function () {
-	console.log("It works");
-})();
+/* ==========  Variables  ========== */
+const control = document.querySelector(".controls");
+
+/* ==========  Functions  ========== */
+function handleUpdate(event) {
+	const suffix = event.target.dataset.sizing ? event.target.dataset.sizing : "";
+	document.documentElement.style.setProperty(
+		`--${event.target.name}`,
+		event.target.value + suffix
+	);
+}
+
+/* ==========  Inits and Event Listeners  ========== */
+control.addEventListener("change", handleUpdate);
+control.addEventListener("mousemove", handleUpdate);
