a052ac7ce8c128607906995b76dc013cea96199f
dwhenson <dave.henson@gmail.com>
2020-09-16 10:18:09 +0300

    test followng total reinstall

--- 04 - Array Cardio Day 1/main.js
+++ 04 - Array Cardio Day 1/main.js
@@ -56,7 +56,7 @@ const people = [
 	"Blair, Tony",
 	"Blake, William",
 ];
-
+// add some text to test
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
 
