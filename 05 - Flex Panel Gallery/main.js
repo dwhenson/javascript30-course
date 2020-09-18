277f26f4b736f1c05278a608567eddb3c5270abb
dwhenson <dave.henson@gmail.com>
2020-09-18 17:07:50 +0300

    🐛 BUG: solution day 4 - issue with removing two classes
    forEach loop to remove .open and .open-active only removes .open.

--- 05 - Flex Panel Gallery/main.js
+++ 05 - Flex Panel Gallery/main.js
@@ -1,1 +1,26 @@
-console.log("it works");
+/* ==========  Variables  ========== */
+const panels = [...document.querySelectorAll(".panel")];
+
+/* ==========  Functions  ========== */
+function resetPanels() {
+	panels.forEach((panel) => {
+		panel.classList.remove("open");
+		panel.classList.remove("open-active");
+	});
+}
+
+function transitionHandler(event) {
+	if (!event.propertyName.includes("flex")) return;
+	const panel = event.target.closest(".panel");
+	panel.classList.toggle("open-active");
+}
+
+function clickHandler(event) {
+	resetPanels();
+	const panel = event.target.closest(".panel");
+	panel.classList.toggle("open");
+}
+
+/* ==========  Inits and Event Listeners  ========== */
+document.addEventListener("click", clickHandler);
+document.addEventListener("transitionend", transitionHandler);
