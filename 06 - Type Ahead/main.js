e80d6c0248ff02a29eaddb2e28e27a6bcb2322e4
dwhenson <dave.henson@gmail.com>
2020-09-19 14:57:15 +0300

    ⚡ ADD: working fetch data and error handling

--- 06 - Type Ahead/main.js
+++ 06 - Type Ahead/main.js
@@ -1,2 +1,22 @@
+/* ==========  Variables  ========== */
 const endpoint =
+	// eslint-disable-next-line max-len
 	"https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
+/* ==========  Functions  ========== */
+
+function renderHTML(locations) {
+	console.log(locations);
+}
+
+async function getData() {
+	const data = await fetch(endpoint);
+	const response = await data.json();
+	renderHTML(response);
+}
+
+function handleError() {
+	console.warn("Oh no, got an error!");
+}
+/* ==========  Inits nd Event Listeners  ========== */
+getData().catch(handleError);
