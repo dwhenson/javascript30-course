92ee3b37dc77a2e8c1f81bc215ca768908f96f0c
dwhenson <dave.henson@gmail.com>
2020-09-21 21:51:46 +0300

    🚀 RELEASE: final working solution day 3

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -1,22 +1,19 @@
-// Avoid global scope
-(function () {
-	/* ==========  Functions  ========== */
-	const keys = document.querySelector(".keys");
-	function keyHandler(event) {
-		const audio = document.querySelector([`audio[data-key="${event.which}"]`]);
-		const key = document.querySelector([`div[data-key="${event.which}"]`]);
-		if (!audio) return;
-		audio.currentTime = 0;
-		audio.play();
-		key.classList.add("playing");
-	}
+/* ==========  Functions  ========== */
+const keys = document.querySelector(".keys");
+function keyHandler(event) {
+	const audio = document.querySelector([`audio[data-key="${event.which}"]`]);
+	const key = document.querySelector([`div[data-key="${event.which}"]`]);
+	if (!audio) return;
+	audio.currentTime = 0;
+	audio.play();
+	key.classList.add("playing");
+}
 
-	function removeTransition(event) {
-		if (!event.propertyName === "transform") return;
-		event.target.classList.remove("playing");
-	}
+function removeTransition(event) {
+	if (!event.propertyName === "transform") return;
+	event.target.classList.remove("playing");
+}
 
-	/*= =======  Inits and Event Listeners  ========== */
-	document.addEventListener("keydown", keyHandler);
-	keys.addEventListener("transitionend", removeTransition);
-})();
+/*= =======  Inits and Event Listeners  ========== */
+document.addEventListener("keydown", keyHandler);
+keys.addEventListener("transitionend", removeTransition);
