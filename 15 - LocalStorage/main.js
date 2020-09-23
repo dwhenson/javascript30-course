410af3a8c6f8783e60c1c00263328f4789c017d5
dwhenson <dave.henson@gmail.com>
2020-09-23 16:55:44 +0300

    ⚡ ADD: checkbox status saved in localStorage

--- 15 - LocalStorage/main.js
+++ 15 - LocalStorage/main.js
@@ -5,24 +5,43 @@ const addItems = document.querySelector(".add-items");
 const storedPrefix = "storedItems";
 
 /* ==========  Functions  ========== */
-function renderHTML(itemsToRender) {
-	itemsList.innerHTML = Object.entries(itemsToRender)
-		.map(([key, value]) => {
-			console.log([key, value]);
-			return `<li>${key}</li>`;
+function renderHTML(itemsToRender, insertLocation) {
+	insertLocation.innerHTML = itemsToRender
+		.map((listItem, index) => {
+			return `
+				<li>
+					<input type="checkbox" data-name=${index} id="item${index}"
+					${listItem.done ? "checked" : ""} />
+        			<label for="item${index}">${listItem.text}</label>
+				</li>`;
 		})
 		.join("");
 }
 
-function updateStorage(itemToAdd) {
-	let existing = localStorage.getItem(storedPrefix);
-	existing = existing ? JSON.parse(existing) : {};
-	existing[itemToAdd] = {
-		type: itemToAdd,
-		checkbox: false,
+function updateStorage(itemValue) {
+	let data = localStorage.getItem(storedPrefix);
+	data = data ? JSON.parse(data) : [];
+	const itemToAdd = {
+		text: itemValue,
+		done: false,
 	};
-	renderHTML(existing);
-	localStorage.setItem(storedPrefix, JSON.stringify(existing));
+	data.push(itemToAdd);
+	renderHTML(data, itemsList);
+	localStorage.setItem(storedPrefix, JSON.stringify(data));
+}
+
+function clickHandler(event) {
+	let data = localStorage.getItem(storedPrefix);
+	data = data ? JSON.parse(data) : [];
+	const index = event.target.getAttribute("data-name");
+	if (!data[index]) return;
+	if (data[index].done) {
+		data[index].done = false;
+	} else {
+		data[index].done = true;
+	}
+	renderHTML(data, itemsList);
+	localStorage.setItem(storedPrefix, JSON.stringify(data));
 }
 
 function submitHandler(event) {
@@ -36,10 +55,11 @@ function submitHandler(event) {
 function loadItems() {
 	let savedItems = localStorage.getItem(storedPrefix);
 	if (!savedItems) return;
-	savedItems = savedItems ? JSON.parse(savedItems) : {};
-	renderHTML(savedItems);
+	savedItems = savedItems ? JSON.parse(savedItems) : [];
+	renderHTML(savedItems, itemsList);
 }
 
 /* ==========  Inits and Event Listeners  ========== */
 loadItems();
 addItems.addEventListener("submit", submitHandler);
+itemsList.addEventListener("click", clickHandler);
