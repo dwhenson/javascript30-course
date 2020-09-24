8dbe0405910f542081175351797bcbcb6b200516
dwhenson <dave.henson@gmail.com>
2020-09-24 17:38:23 +0300

    🚀 RELEASE: final working solution day 17

---
+++ 17 - Sort Without Articles/main.js
@@ -0,0 +1,36 @@
+/* ==========  Variables  ========== */
+const list = document.querySelector("#bands");
+const bands = [
+	"The Plot in You",
+	"The Devil Wears Prada",
+	"Pierce the Veil",
+	"Norma Jean",
+	"The Bled",
+	"Say Anything",
+	"The Midway State",
+	"We Came as Romans",
+	"Counterparts",
+	"Oh, Sleeper",
+	"A Skylit Drive",
+	"Anywhere But Here",
+	"An Old Dog",
+];
+
+/* ==========  Functions  ========== */
+
+function strip(bandName) {
+	return bandName.replace(/^(a |the |an )/i, "").trim();
+}
+
+const sortedBands = bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
+
+function renderHTML(array) {
+	list.innerHTML = array
+		.map((band) => {
+			return `<li>${band}</li>`;
+		})
+		.join("");
+}
+
+/* ==========  Inits and Event Listeners  ========== */
+renderHTML(sortedBands);
